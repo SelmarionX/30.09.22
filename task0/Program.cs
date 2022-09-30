5f2dc321c6ffff7ec921506256f908d619d1e54c
unknown <selmarionx@gmail.com>
2022-09-30 20:31:16 +0300

    добавили первый пример

--- task0/Program.cs
+++ task0/Program.cs
@@ -4,3 +4,7 @@
 // 4 -> 16 
 // -3 -> 9 
 // -7 -> 49
+Console.Write("Введите число ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number*number;
+Console.WriteLine("Квадрат числа" + number + "равен =  " + sqr);
