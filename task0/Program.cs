0d9d15d20faac8250a1516d61eb224b591e9695a
unknown <selmarionx@gmail.com>
2022-10-01 19:45:12 +0300

    поправили вывод текста

--- task0/Program.cs
+++ task0/Program.cs
@@ -7,4 +7,4 @@
 Console.Write("Введите число ");
 int number = int.Parse(Console.ReadLine());
 int sqr = number*number;
-Console.WriteLine("Квадрат числа" + number + "равен =  " + sqr);
+Console.WriteLine("Квадрат числа " + number + " равен =  " + sqr);
