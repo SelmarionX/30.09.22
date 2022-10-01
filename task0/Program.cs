f59976b1fda94773190a9dc6267df92941ae89f3
unknown <selmarionx@gmail.com>
2022-10-01 20:08:59 +0300

    добавили task2

--- task0/Program.cs
+++ task0/Program.cs
@@ -6,5 +6,5 @@
 // -7 -> 49
 Console.Write("Введите число ");
 int number = int.Parse(Console.ReadLine());
-int sqr = number*number;
+int sqr = number * number;
 Console.WriteLine("Квадрат числа " + number + " равен =  " + sqr);
--- task1/Program.cs
+++ task1/Program.cs
@@ -1,10 +1,20 @@
-﻿// Напишите программу, которая на вход принимает число и 
-// выдаёт его квадрат (число умноженное на само себя).
-// Например:
-// 4 -> 16 
-// -3 -> 9 
-// -7 -> 49
-Console.Write("Введите число: ");
-int number = int.Parse(Console.ReadLine());
-int sqr = number * number;
-Console.WriteLine("Квадрат числа " + number + " равен = " + sqr);
+﻿
+//1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+//a = 25, b = 5 -> да
+//a = 2, b = 10 -> нет
+//a = 9, b = -3 -> да
+//a = -3 b = 9 -> нет
+
+Console.Write("Введите число ");
+int numberOne = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int numberTwo = int.Parse(Console.ReadLine());
+
+if(numberOne == numberTwo*numberTwo)
+{
+Console.Write(" Да ");
+}
+else
+{
+Console.Write(" нет ");
+}
